1cb1677cfa842bf918892ba3696e34bfe0449b9c
saba_guledani <saba.guledani.1@btu.edu.ge>
2023-03-22 17:06:28 +0400

    second commit

---
+++ app/src/main/java/com/example/onroad/adapters/RecyclerviewAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.onroad.adapters
+
+class RecyclerviewAdapter() {
+}
--- app/src/main/java/com/example/onroad/classes/Driver.kt
+++ app/src/main/java/com/example/onroad/classes/Driver.kt
@@ -1,5 +1,20 @@
 package com.example.onroad.classes
 
 class Driver {
+    var name:String = "name"
+    var car:String = "car model"
+    var service:String = "tour guide or driver"
+    var rating:Int = 0
 
+    constructor()
+    constructor(name:String,
+                car:String,
+                service:String,
+                rating:Int){
+        this.name = name
+        this.car = car
+        this.service= service
+        this.rating = rating
+
+    }
 }
