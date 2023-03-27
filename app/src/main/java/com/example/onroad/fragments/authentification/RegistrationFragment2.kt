c29e843a8b9175b179e67990ada53668c04c1d51
saba_guledani <saba.guledani.1@btu.edu.ge>
2023-03-27 10:44:55 +0400

    language,auth update(50%) 2

--- app/src/main/java/com/example/onroad/fragments/authentification/RegistrationFragment2.kt
+++ app/src/main/java/com/example/onroad/fragments/authentification/RegistrationFragment2.kt
@@ -5,56 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.onroad.R
+import com.example.onroad.databinding.FragmentRegistration2Binding
+import com.google.firebase.database.DatabaseReference
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class RegistrationFragment2 : Fragment(R.layout.fragment_registration2) {
+    private lateinit var binding: FragmentRegistration2Binding
+    private lateinit var dbref: DatabaseReference
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistrationFragment2.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RegistrationFragment2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onResume() {
+        super.onResume()
+        val languages = resources.getStringArray(R.array.languages)
+        val arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item,languages)
+        binding.autocompletetext.setAdapter(arrayAdapter)
+        binding.autocompletetext2.setAdapter(arrayAdapter)
+        binding.autocompletetext3.setAdapter(arrayAdapter)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegistration2Binding.bind(view)
+
+        binding.addLanguages.setOnClickListener{
+            if(binding.autocompletetext2.visibility==View.GONE && binding.autocompletetext3.visibility==View.GONE){
+                binding.autocompletetext2.visibility = View.VISIBLE
+                binding.addLanguage2.visibility = View.VISIBLE
+            }else if(binding.autocompletetext2.visibility == View.VISIBLE && binding.autocompletetext3.visibility==View.GONE ){
+                binding.autocompletetext3.visibility = View.VISIBLE
+                binding.addLanguage3.visibility = View.VISIBLE
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration2, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationFragment2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationFragment2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
