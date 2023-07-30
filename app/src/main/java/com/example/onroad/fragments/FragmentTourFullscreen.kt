4afea5259c5b46a220b91a55c82359e59dcfdfb0
saba_guledani <saba.guledani.1@btu.edu.ge>
2023-07-30 15:18:00 +0400

    before compose

--- app/src/main/java/com/example/onroad/fragments/FragmentTourFullscreen.kt
+++ app/src/main/java/com/example/onroad/fragments/FragmentTourFullscreen.kt
@@ -1,19 +1,17 @@
 package com.example.onroad.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.SnapHelper
 import com.example.onroad.R
 import com.example.onroad.adapters.CarouselAdapter
 import com.example.onroad.databinding.FragmentTourFullscreenBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.carousel.CarouselLayoutManager
 import com.google.android.material.carousel.CarouselSnapHelper
 import com.google.android.material.carousel.FullScreenCarouselStrategy
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.elevation.SurfaceColors
 
 
 class FragmentTourFullscreen : Fragment(R.layout.fragment_tour_fullscreen) {
@@ -33,13 +31,20 @@ class FragmentTourFullscreen : Fragment(R.layout.fragment_tour_fullscreen) {
 
         val recyclerview = binding.recyclerView
 
+        val colorSurface1 = SurfaceColors.SURFACE_1.getColor(requireContext())
+        val colorSurface2 = SurfaceColors.SURFACE_2.getColor(requireContext())
+        val colorSurface3 = SurfaceColors.SURFACE_3.getColor(requireContext())
+        val colorSurface4 = SurfaceColors.SURFACE_4.getColor(requireContext())
+        val colorSurface5 = SurfaceColors.SURFACE_5.getColor(requireContext())
+
 
         recyclerview.layoutManager = CarouselLayoutManager(FullScreenCarouselStrategy())
         val snapHelper: SnapHelper = CarouselSnapHelper()
         snapHelper.attachToRecyclerView(recyclerview)
 
 
-
+        binding.tableLayout.setBackgroundResource(R.drawable.shape_rounded)
+        binding.tableLayout.background.setTint(colorSurface5)
         imgList = ArrayList()
 
         imgList.add(R.drawable.martvili)
